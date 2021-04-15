e653fe4ce2d60b6ff416f5d799334ce2e87fb17f
Rares Sandu <59281487+RaresSanduConstantin@users.noreply.github.com>
2021-04-15 09:20:54 +0300

    remove gitignore files

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,9 +8,25 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51GwtELJltgc2LzsHnCdnKR5OQiAD8BhfJ1bBFUMmjupZEkTZrtZ0idSMA6KHmlxfefI8vkm2Y5oQXC8gE3zoG02W00FOYTLdQt";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Succesful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("succesful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
+
   return (
     <StripeCheckout
       currency="RON"
